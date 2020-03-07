1bde9e14f16cfba153d85ea17cabd9944a0a8b20
sparshypop7 <sparshdpatel@utexas.edu>
2020-03-06 18:03:25 -0600

    Lab 2 done

--- BlynkStarter/BlynkStarter.ino
+++ BlynkStarter/BlynkStarter.ino
@@ -31,32 +31,106 @@
 
 /* Comment this out to disable prints and save space */
 #define BLYNK_PRINT Serial
+int LED = 2;
 
 
+#include <Blynk.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
+// You will need to define a timer object. BlynkTimer uses your MCU hardware to create a timer
+// that is compatible with all the Blynk events.
+BlynkTimer timer;
+
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "Your token here";
+char auth[] = "eFpTZlK5rc-lErYZgNi8itc755G3LJRZ";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 // The EE IOT network is hidden. You might not be able to see it.
 // But you should be able to connect with these credentials. 
-char ssid[32] = "EE-IOT-Platform-02";
-char pass[32] = "g!TyA>hR2JTy";
+char ssid[32] = "utguest";
+char pass[32] = "";
 
-void setup()
-{
+const int freq = 5000;     // 5KHz frequency is more than enough. Remember we used 100 before.
+const int ledChannel = 2;  // You can have up to 16 pwm channels (0 to 15)
+const int resolution = 10; // 10 bit resolution for 1023. Freq should be greater than resolution.
+
+int time_count = 1000; // timer counter global variable
+String content = "";  // null string constant ( an empty string )
+
+void setup(){
+
+  pinMode(LED, OUTPUT);
   // Serial Monitor
   Serial.begin(115200);
   Blynk.begin(auth, ssid, pass);
+
+  // configure LED PWM functionality
+  ledcSetup(ledChannel, freq, resolution);
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(LED, ledChannel);
+
+    // Setup a function to be called every second
+    timer.setInterval(10L, myTimerEvent);
+}
+// This function sends Arduino's up time every second to Virtual Pin 0.
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+// You can send any value at any time.
+// Don't send more than 10 values a second or the Blynk server will block you!
+    if (time_count % 25 == 0) { // every 250 ms
+        // Do thing that needs to happen every 0.25 seconds
+    }
+    if (time_count % 100 == 0){
+        // Do thing that needs to happen every 1 second
+        time_count = 0; // reset time counter
+    }
+    else {
+        // Send serial data to Blynk terminal
+        char character;
+        while(Serial.available()) { // Check if serial is available every 10 ms
+            character = Serial.read();
+            content.concat(character);
+        }
+        if (content != "") {
+            Blynk.virtualWrite(V3, content);
+            content = ""; // Clear String
+        }  
+    }
+    time_count += 1; // Increment on every tick
+}
+
+// This function executes whenever V1 is written to.
+// You don't need to call it. Blynk handles the trigger for you.
+BLYNK_WRITE(V1)
+{
+
+    // param is a member variable of the Blynk ADT. It is exposed so you can read it.
+    int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+
+    // Because V1 is a button, pinValue will be a 0 or a 1.
+    if (pinValue == 0) {
+        digitalWrite(LED, LOW);   // turn the LED off (LOW is the voltage level)
+    }
+    else {
+        digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+    }
+    ledcWrite(ledChannel, pinValue); // Note that this takes ledChannel as an argument, NOT the pin! Set duty = val.
+}
+// This function is executed whenever Blynk pin V5 requests data from the ESP32
+BLYNK_READ(V5) // Widget in the app READS Virtal Pin V5 with the certain frequency
+{
+    // This command writes Arduino's uptime in seconds to Virtual Pin V5
+    Blynk.virtualWrite(5, millis() / 1000);
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run(); // Initiates BlynkTimer
 }
-
--- Report.md
+++ Report.md
@@ -1,43 +1,44 @@
-Name:
+Name: Sparsh Patel
 
-EID:
+EID: sdp2557
 
-Team Number:
+Team Number: F12
 
 ## Questions
 
 1. What is the purpose of an IP address?
 
-    answer
+    IP addresses are addresses that specify what embedded device is connected to the Internet.
 
 2. What is a DNS? What are the benefits of using domain names instead of IP addresses?
 
-    answer
+    DNS, or Domain Name Service, is a linkage between a symbolic name and its corresponding IP address.
+Benefits of using DNS compared to an IP address is that it has a symbolic name which is easier to write out for the user than an IP address.
 
 3. What is the difference between a static IP and a dynamic IP?
 
-    answer
+    Static IP addresses do not change when being used, dynamic IP address change over time as it is connected to a device.
 
 4. What is the tradeoff between UDP and TCP protocols?
 
-    answer
+    UDP is fast, but not reliable while TCP is reliable, but not fast.
 
 5. Why can't we use the delay function with Blynk?
 
-    answer
+     We cant use the delay function with Blynk because it will mess up the timing and break it since Blynk is event based.
 
 6. What does it mean for a function to be "Blocking"?
 
-    answer
+    A fuction to be "Blocking" would mean that nothing else is funning, which would waste time to be spent elsewhere.
 
 7. Why are interrupts useful for writing Non-Blocking code?
 
-    answer
+    Interrupts are effective for writing Non-Blocking code because it will run immediately, regardless of what program of code is running.
 
 8. What is the difference between interface and implementation? Why is it important?
 
-   answer
+   Interface defines how a function is called vs implementation is what the function does. This is important to understanding how a program and a user communicate.
 
 9. Screenshot of your Blynk App:
 
-    ![your image here->](img/placeholder.png)
+    (img/Lab2image.png)
